fbb50512a87b5bdc5b49c0ea414be175e0f80595
lvzhe <lvzhe@maxtropy.com>
2019-03-03 15:40:39 +0800

    feat: 1.1.2

--- bin/index.js
+++ bin/index.js
@@ -3,8 +3,10 @@
 const commander = require("commander");
 const chalk = require("chalk");
 const path = require("path");
+const ora = require('ora');
+const download = require("download-git-repo");
 const fs = require("fs-extra");
-const shelljs = require("shelljs");
+const symbols = require('log-symbols');
 const pkg = require("../package");
 const config = require("../config");
 
@@ -17,19 +19,31 @@ commander.on("--help", function(){
    console.log("  create app with react hooks");
    console.log("");
    console.log("Example:");
-   console.log("  create-react-zlv <your_project_name_here>");
-   console.log("");
+   console.log(`  create-react-zlv -i ${chalk.green("<your_project_name_here>")}`);
    console.log("");
 });
 
 commander.parse(process.argv);
 
 if (commander.init) {
+    const spinner = ora('正在初始化项目...');
     const { gitUrl, branch } = config;
-    var projectPath = path.resolve(commander.init);
-    var projectName = path.win32.basename(projectPath);
-    console.log(`${chalk.red("init project in ")}${chalk.green(projectName)}`);
-    console.log(gitUrl, branch, projectName);
-    shelljs.exec(`git clone ${gitUrl} ${projectName} && cd ${projectName} && git checkout ${branch}`);
-}
+    let projectPath = path.resolve(commander.init);
+    const projectName = path.win32.basename(projectPath);
+    if (!fs.existsSync(projectName)) {
+        console.log(`${chalk.red("Init project in:  ")}${chalk.green(projectName)}`);
+        spinner.start();
+        download(`direct:${gitUrl}#${branch}`, projectName, { clone: true }, err => {
+            if (err) {
+                spinner.fail();
+                console.log(symbols.error, chalk.red(err));
+            } else {
+                spinner.succeed();
+                console.log(symbols.success, chalk.green('Project initialized successfully'));
 
+            }
+        })
+    } else {
+        console.log(symbols.error, chalk.red("该目录下已存在同名文件夹!"));
+    }
+}
