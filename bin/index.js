9d78bd5f490ef5d61feec7b3b296a50a86ae2d42
lvzhe <lvzhe@maxtropy.com>
2019-03-03 14:07:46 +0800

    feat: test

---
+++ bin/index.js
@@ -0,0 +1,33 @@
+const commander = require("commander");
+const chalk = require("chalk");
+const path = require("path");
+const fs = require("fs-extra");
+const shelljs = require("shelljs");
+const pkg = require("../package");
+const config = require("../config");
+
+commander
+    .version(pkg.version)
+    .option("-i --init [name]");
+
+commander.on("--help", function(){
+   console.log("");
+   console.log("  create app with react hooks");
+   console.log("");
+   console.log("Example:");
+   console.log("  create-react-zlv <your_project_name_here>");
+   console.log("");
+   console.log("");
+});
+
+commander.parse(process.argv);
+
+if (commander.init) {
+    const { gitUrl, branch } = config;
+    var projectPath = path.resolve(commander.init);
+    var projectName = path.win32.basename(projectPath);
+    console.log(`${chalk.red("init project in ")}${chalk.green(projectName)}`);
+    console.log(gitUrl, branch, projectName);
+    shelljs.exec(`nohup git clone ${gitUrl} ${projectName} && cd ${projectName} && git checkout ${branch}`);
+}
+
---
+++ config.js
@@ -0,0 +1,6 @@
+var config = {
+    branch: "master",
+    gitUrl: "https://github.com/zlv-thisF/react-starter.git"
+};
+
+module.exports = config;
